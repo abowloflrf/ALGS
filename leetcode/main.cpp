7816604a6a0072d55f085c14e33c18c1e4bed89f
abowloflrf <lrf0829@126.com>
2018-04-09 00:24:55 +0800

    leetcode 013

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,19 +1,11 @@
 // #include "src/009PalindromeNumber.cpp"
-#include "src/007ReverseInteger.cpp"
+#include <string>
+#include "src/013RomanToInteger.cpp"
 
 int main() {
     Solution test = Solution();
-    // bool a = test.isPalindrome(123);
-    // bool c = test.isPalindrome(123321);
-    // bool d = test.isPalindrome(12321);
-    // bool e = test.isPalindrome(-123321);
-    // bool f = test.isPalindrome(1147483648);
-    // bool g = test.isPalindrome(1234554321);
-    int a = test.reverse(123);
-    int b = test.reverse(120);
-    int c = test.reverse(-333);
-    int d = test.reverse(-550);
-    int e = test.reverse(-2147483412);
-    int f = test.reverse(1534236469);
+    int a = test.romanToInt("XXI");   // 21
+    int b = test.romanToInt("XIV");   // 14
+    int c = test.romanToInt("XCIX");  // 99
     return 0;
 }
---
+++ leetcode/src/013RomanToInteger.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+#include <string>
+#include <unordered_map>
+class Solution {
+   public:
+    int roman(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+    int romanToInt(std::string s) {
+        std::unordered_map<char, int> flag = {
+            {'I', 1},   {'V', 5},   {'X', 10},  {'L', 50},
+            {'C', 100}, {'D', 500}, {'M', 1000}};
+        int len = s.size(), result = 0;
+        for (int i = len - 1; i >= 0; i--) {
+            //先加第一位
+            if (i == len - 1) {
+                result += flag[s[i]];
+                continue;
+            }
+            //大于第二位时，若这一位比上一位的数字小则应该相减
+            if (flag[s[i]] < flag[s[i + 1]]) {
+                result -= flag[s[i]];
+            } else {
+                result += flag[s[i]];
+            }
+        }
+        return result;
+    }
+    int romanToInt2(std::string s) {
+        int result = 0;
+        for (int i = s.length() - 1; i >= 0; i--) {
+            //先加第一位
+            if (i == s.length() - 1) {
+                result += roman(s[i]);
+                continue;
+            }
+            //大于第二位时，若这一位比上一位的数字小则应该相减
+            if (roman(s[i]) < roman(s[i + 1])) {
+                result -= roman(s[i]);
+            } else {
+                result += roman(s[i]);
+            }
+        }
+        return result;
+    }
+};
