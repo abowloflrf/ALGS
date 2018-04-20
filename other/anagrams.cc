f97baef737518ab31cc05c05669c56856aab9b91
ruofeng <lrf0829@126.com>
2018-04-20 09:29:56 +0800

    problem describe

--- other/anagrams.cc
+++ other/anagrams.cc
@@ -1,3 +1,7 @@
+/**
+ * 给出一份字典包含若干个英文单词，要求按照这样的规则将这份单词分组：
+ * 单词出现的字母相同且每个字母出现的次数相同分为一组，例如cat/act
+ */
 #include <algorithm>
 #include <iostream>
 #include <string>
@@ -20,6 +24,6 @@ int main() {
     vector<string> data = {"act", "ate",    "tea",     "buffer",
                            "cat", "string", "anagram", "nagaram"};
     unordered_map<string, vector<string>> ret = solution(data);
-    
+
     return 0;
 }
