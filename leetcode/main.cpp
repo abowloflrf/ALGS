a21f7b2b475361b642724deb482fdf9b6cd44a1e
ruofeng <lrf0829@126.com>
2018-05-29 00:57:21 +0800

    leetcode 021

--- leetcode/main.cpp
+++ leetcode/main.cpp
@@ -1,10 +1,26 @@
 #include <vector>
-#include "src/026RemoveDuplicatesFromSortedArray.cc"
+#include "src/021MergeTwoSortedLists.cc"
 using namespace std;
-int main()
-{
+int main() {
     Solution s = Solution();
-    vector<int> vec = {1,1,2,2,2,3,3,3,4,5,6,8,8,8};
-    int num = s.removeDuplicates2(vec);
+    ListNode *list1;
+    list1 = (ListNode *)malloc(sizeof(ListNode));
+    list1->val = 9;
+    list1->next = (ListNode *)malloc(sizeof(ListNode));
+    list1->next->val = 20;
+    list1->next->next = (ListNode *)malloc(sizeof(ListNode));
+    list1->next->next->val = 33;
+
+    ListNode *list2;
+    list2 = (ListNode *)malloc(sizeof(ListNode));
+    list2->val = 1;
+    list2->next = (ListNode *)malloc(sizeof(ListNode));
+    list2->next->val = 3;
+    list2->next->next = (ListNode *)malloc(sizeof(ListNode));
+    list2->next->next->val = 4;
+
+    ListNode *resultList;
+    resultList = s.mergeTwoLists(list1, list2);
+
     return 0;
 }
--- leetcode/src/026RemoveDuplicatesFromSortedArray.cc
+++ leetcode/src/026RemoveDuplicatesFromSortedArray.cc
@@ -1,20 +1,15 @@
-#include <vector>
 #include <iostream>
+#include <vector>
 
 using namespace std;
 
-class Solution
-{
-  public:
-    int removeDuplicates(vector<int> &nums)
-    {
-        if (nums.empty())
-            return 0;
+class Solution {
+   public:
+    int removeDuplicates(vector<int> &nums) {
+        if (nums.empty()) return 0;
 
-        for (auto iter = nums.begin() + 1; iter != nums.end(); iter++)
-        {
-            if (*iter == *(iter - 1))
-            {
+        for (auto iter = nums.begin() + 1; iter != nums.end(); iter++) {
+            if (*iter == *(iter - 1)) {
                 nums.erase(iter);
                 iter--;
             }
