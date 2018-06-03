06571aca7ef95ffe152e69640c3ee3e566f3d426
abowloflrf <lrf0829@126.com>
2018-06-03 16:24:32 +0800

    lintcode 0053

---
+++ lintcode/0053-reverse-words-in-a-string.cc
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Solution {
+   public:
+    /*
+     * @param s: A string
+     * @return: A string
+     */
+    string reverseWords(string &s) {
+        // write your code here
+        int size = s.size();
+        string res = "";
+        string appendStr = "";
+        for (int i = 0; i < size; i++) {
+            //清除头部空字符
+            if (appendStr.empty() && s[i] == ' ') continue;
+            if (s[i] == ' ') {
+                if (appendStr.empty())
+                    continue;
+                else {
+                    res = (res.empty() ? appendStr : appendStr + " ") + res;
+                    appendStr = "";
+                }
+            } else {
+                appendStr += s[i];
+            }
+        }
+        if (!appendStr.empty()) res = appendStr + " " + res;
+        return res;
+    }
+};
+
+int main() {
+    Solution s = Solution();
+    string str = "  i love   you";
+    string res = s.reverseWords(str);
+    cout << res << endl;
+    return 0;
+}
