bd69f6088f870fe021704d459502326c95ef8235
abowloflrf <lrf0829@126.com>
2018-07-22 13:12:02 +0800

    heap sort

---
+++ base/sort/HeapSort.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void maxHeapify(vector<int>& data, int i, int n) {
+    //得到左右的索引
+    int indexMax = i, indexLeft = 2 * i + 1, indexRight = 2 * (i + 1);
+
+    //得到左右与跟哪个值最大
+    if (indexLeft < n && data[i] < data[indexLeft]) {
+        indexMax = indexLeft;
+    }
+    if (indexRight < n && data[indexMax] < data[indexRight]) {
+        indexMax = indexRight;
+    }
+    //若左右有大于根节点的则作交换并递归调用
+    if (indexMax != i) {
+        swap(data[indexMax], data[i]);
+        maxHeapify(data, indexMax, n);
+    }
+}
+
+void heapSort(vector<int>& data) {
+    // 从顶部开始构建堆
+    int heapSize = data.size();
+    for (int i = heapSize / 2 - 1; i >= 0; i--) maxHeapify(data, i, heapSize);
+
+    for (int i = heapSize - 1; i >= 0; i--) {
+        //首尾交换，断尾重建堆，每次将大根堆的顶部放到最后然后i--表示重建时不管最后一个元素看
+        //因为它已经是整个数组中最大的元素
+        swap(data[0], data[i]);
+        maxHeapify(data, 0, i);
+    }
+}
+
+int main(int argc, char const* argv[]) {
+    vector<int> data = {16, 4, 10, 14, 7, 9, 3, 2, 8, 1};
+    heapSort(data);
+    for (auto i : data) {
+        cout << i << "-";
+    }
+    return 0;
+}
